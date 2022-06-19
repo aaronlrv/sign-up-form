30d3cd6ae37e9ec791c1e66dddae5bc97ab64bd9
aaronlrv <aaronvitharanage@gmail.com>
2022-06-19 23:50:32 +0400

    Rework validation function to work base off form submission

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 let button = document.querySelector("#submit")
 
-function passwordValidation (password1,password2){
-    if (password1 === password2){
-        e.preventDefault()
+function passwordValidation (e){
+    event.preventDefault()
+    let password = document.getElementById("password").value
+    let confirmPassword = document.getElementById("confirm").value
+
+    console.log(password)
+    console.log(confirmPassword)
+    if (password === confirmPassword){
         alert("Great! Password is correct")
     } else {
-        e.preventDefault()
         alert("Uh oh, Your passwords dont match!")
     }
 }
-
-button.addEventListener,("click", () => {
-    const password = document.getElementById("password").value
-    const confirmPassword = document.getElementById("confirm").value
-
-    passwordValidation(password,confirmPassword)
-    e.preventDefault()
-})
